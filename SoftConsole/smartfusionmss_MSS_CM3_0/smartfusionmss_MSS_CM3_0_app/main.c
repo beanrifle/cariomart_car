b38110eb31e6a460856a945538aa08b54dae3be8
youngben <youngben@umich.edu>
2019-12-02 20:04:16 -0500

    Added packet reception functionality and updating of speed

--- SoftConsole/smartfusionmss_MSS_CM3_0/smartfusionmss_MSS_CM3_0_app/main.c
+++ SoftConsole/smartfusionmss_MSS_CM3_0/smartfusionmss_MSS_CM3_0_app/main.c
@@ -2,16 +2,22 @@
 #include <inttypes.h>
 #include <string.h>
 
-#include "CarControl.h"
 #include "drivers/mss_uart/mss_uart.h"
+#include "drivers/mss_gpio/mss_gpio.h"
+
+#include "CarControl.h"
 #include "xbee.h"
 
 
 
 int main() {
+	; // state of forward/reverse buttons
+	int race=0;
 	initialize_controller_reciever();
 
-	setLeftSideForward();
+
+
+	/*setLeftSideForward();
 	setRightSideForward();
 
 	setLeftSideSpeed(64);
@@ -25,6 +31,7 @@ int main() {
 
 	setLeftSideSpeed(0);
 	setRightSideSpeed(0);
+	*/
 
 	while(1){
 
--- SoftConsole/smartfusionmss_MSS_CM3_0/smartfusionmss_MSS_CM3_0_app/xbee.c
+++ SoftConsole/smartfusionmss_MSS_CM3_0/smartfusionmss_MSS_CM3_0_app/xbee.c
@@ -1,31 +1,54 @@
 #include "xbee.h"
+#include "CarControl.h"
 #include <stdio.h>
+
+int button_state=0;
 
 void initialize_controller_reciever(void) {
 	MSS_UART_init(&g_mss_uart1, MSS_UART_9600_BAUD, MSS_UART_DATA_8_BITS | MSS_UART_NO_PARITY | MSS_UART_ONE_STOP_BIT);
-	MSS_UART_set_rx_handler( &g_mss_uart1, uart1_rx_handler, MSS_UART_FIFO_EIGHT_BYTES);
+	MSS_UART_set_rx_handler( &g_mss_uart1, uart1_rx_handler, MSS_UART_FIFO_SINGLE_BYTE);
 }
 
 void uart1_rx_handler( mss_uart_instance_t * this_uart ) {
-	uint8_t rx_buff[8];
+	char rx_buff[1];
 	char recieved_data[50];
-	int rx_size = MSS_UART_get_rx( this_uart, rx_buff, sizeof(rx_buff) );
-	char current_char = rx_buff[0];
-	int i = 0, chr_index = 0;
-	while (current_char != 10) {
-		if (chr_index >= rx_size || chr_index == 8) {
-			rx_size = MSS_UART_get_rx( this_uart, rx_buff, sizeof(rx_buff) );
-			chr_index = 0;
-			continue;
-		}
-		recieved_data[i] = current_char;
-		i++;
-		chr_index++;
-		current_char = rx_buff[chr_index];
-	}
-	recieved_data[i] = 10;
-	recieved_data[i+1] = 0;
+	int button_state=0;
+	int accel=0;
+	int rx_size = MSS_UART_get_rx( this_uart, (uint8_t*)rx_buff, sizeof(rx_buff) );
+	if (*rx_buff == '\r' || *rx_buff == '\n') rx_size = MSS_UART_get_rx( this_uart, (uint8_t*)rx_buff, sizeof(rx_buff) );
+	if (*rx_buff == '\r' || *rx_buff == '\n') rx_size = MSS_UART_get_rx( this_uart, (uint8_t*)rx_buff, sizeof(rx_buff) );
 
-	puts(recieved_data);
+	int i = 0;
+	while (*rx_buff != '\n' && *rx_buff != '\r' && i < 50) {
+		if (rx_size > 0 ) recieved_data[i++] = *rx_buff;
+		rx_size = MSS_UART_get_rx( this_uart, (uint8_t*)rx_buff, sizeof(rx_buff) );
+	}
+	recieved_data[i] = '\0';
+
+	// Process the packet string recieved data, i is the length of the recieved data
+
+	if(recieved_data[0]=='0'){
+		//Packet structure  = source,button_state,accel_data
+		button_state = int(recieved_data[2]);
+		accel = ;
+		if(button_state == 1){
+			setLeftSideForward();
+			setRightSideForward();
+			setLeftSideSpeed(100 - (335-accel)/2);
+			setRightSideSpeed(100 + (335-accel)/2);
+		}
+		else if(button_state==2){
+			setLeftSideReverse();
+			setRightSideReverse();
+			setLeftSideSpeed(100 - (335-accel)/2);
+			setRightSideSpeed(100 + (335-accel)/2);
+		}
+		else{
+			setLeftSideForward();
+			setRightSideForward();
+			setLeftSideSpeed(0);
+			setRightSideSpeed(0);
+		}
+	}
 
 }
